b997dd2cc79459c4b3ddc8255be48d913b7f363e
Glitch (ponggamejs) <none>
2021-11-19 00:19:04 +0000

    ☄️⛱ Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -1,42 +1,65 @@
+const paddleMargin = 20;
+const paddleHeight = 60;
+const paddleWidth = 20;
+const ballRadius = 10;
+
 function handleKeyEvent(e) {
   if (e.repeat) return;
 
   socket.emit(e.type, e.code);
 }
 
+function handleMessageKeyPress(e) {
+  if (e.code == "Enter") {
+    let message = document.getElementById("message");
+
+    socket.emit("message", message.value);
+    
+    message.value = "";
+  }
+}
+
 var room = "";
-var player1;
-var player2;
+var playerName = "";
+var game;
+var gameContext;
 var size = [];
 
 while (room == "" || room == null || room == undefined) {
-  room = prompt("Room Code:");
+  room = prompt("Room Code: ");
+}
+
+while (playerName == "" || playerName == null || playerName == undefined) {
+  playerName = prompt("Player Name: ");
 }
 
 var socket = io();
 
-socket.emit("setScreenSize", [window.innerWidth, window.innerHeight]);
+socket.emit("setScreenSize", [window.innerWidth - 50, window.innerHeight - 50]);
+
+socket.emit("playername", playerName);
 
 socket.emit("room", room);
 
 socket.on("updateBody", body => {
-  document.body.innerHTML = body;
-  
-  player1 = document.getElementById("player1");
-  player2 = document.getElementById("player2");
+  document.getElementById("mainContent").innerHTML = body;
+
+  game = document.getElementById("game");
+
+  if (game) {
+    gameContext = game.getContext("2d");
+  }
 });
 
 socket.on("setScreenSize", newSize => {
-  let game;
+  while (!game) {}
 
-  while (!game) {
-    game = document.getElementsByClassName("game")[0];
-  }
-  
   size = newSize;
 
-  game.style.height = size[1] + "px";
-  game.style.width = size[0] + "px";
+  console.log(size);
+
+  game.height = size[1];
+  game.width = size[0];
 });
 
 socket.on("playerLeft", () => {
@@ -50,10 +73,58 @@ socket.on("failedJoin", reason => {
 });
 
 socket.on("updatePositions", positions => {
-  if (player1 == null || player2 == null) return;
-  player1.style.top = positions[0] - 30 + "px";
-  player2.style.top = positions[1] - 30 + "px";
+  if (!game) return;
+
+  gameContext.clearRect(0, 0, game.width, game.height);
+
+  gameContext.fillStyle = "#FFFF00";
+
+  gameContext.beginPath();
+  gameContext.arc(positions[2][0], positions[2][1], ballRadius, 0, 2 * Math.PI);
+  gameContext.fill();
+
+  gameContext.fillStyle = "#00FF00";
+
+  gameContext.beginPath();
+  gameContext.rect(
+    paddleMargin,
+    positions[0] - paddleHeight / 2,
+    paddleWidth,
+    paddleHeight
+  );
+  gameContext.fill();
+
+  gameContext.beginPath();
+  gameContext.rect(
+    game.width - paddleWidth - paddleMargin,
+    positions[1] - paddleHeight / 2,
+    paddleWidth,
+    paddleHeight
+  );
+  gameContext.fill();
+});
+
+socket.on("updateUserList", userList => {
+  let output = "";
+
+  document.getElementById("room").innerHTML = room;
+
+  for (let user of userList) {
+    output += "<h4>" + user + "</h4>";
+  }
+
+  document.getElementById("userListContent").innerHTML = output;
+});
+
+socket.on("message", data => {
+  let chatContent = document.getElementById("chatContent");
+  
+  chatContent.innerHTML = "&nbsp;&nbsp;" + data + "<br>" + chatContent.innerHTML;
 });
 
 window.addEventListener("keydown", handleKeyEvent);
 window.addEventListener("keyup", handleKeyEvent);
+
+window.addEventListener("load", () => {
+  document.getElementById("message").addEventListener("keydown", handleMessageKeyPress);
+});
